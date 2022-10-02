77d2d469957a9da7f9c74f5e1bb54f395fcf3739
Michael Jay Zamoras <thisiszammy@gmail.com>
2022-10-02 20:14:40 +0800

    added circle class

---
+++ Week7GroupWork/Entities/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7GroupWork.Entities
+{
+    public class Circle : Shape
+    {
+        public override double CalculateSurfaceArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
