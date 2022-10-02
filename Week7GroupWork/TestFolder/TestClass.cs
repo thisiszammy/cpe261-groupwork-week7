48c77a67c708152f0b860ab0f67151c47cfe3d53
alren3 <alrenvinceav@gmail.com>
2022-10-02 21:30:18 +0800

    Hi I only put TESTRANI method

--- Week7GroupWork/TestFolder/TestClass.cs
+++ Week7GroupWork/TestFolder/TestClass.cs
@@ -8,5 +8,9 @@ namespace Week7GroupWork.TestFolder
 {
     internal class TestClass
     {
+        internal void TESTRANI()
+        {
+            Console.WriteLine("Hello Test Rani");
+        }
     }
 }
