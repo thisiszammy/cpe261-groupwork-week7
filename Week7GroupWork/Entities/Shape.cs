4b20d67af08806731c3e79bb77e8020753a575e6
Michael Jay Zamoras <thisiszammy@gmail.com>
2022-10-02 14:32:34 +0800

    added shape entity

---
+++ Week7GroupWork/Entities/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7GroupWork.Entities
+{
+    public abstract class Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public abstract double CalculateSurfaceArea();   
+    }
+}
