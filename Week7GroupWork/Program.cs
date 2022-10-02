4b24dc93df8a45c88ddf1c3d35929f3b90cb33b5
Kenette John <antonio.kenjohn15@gmail.com>
2022-10-02 20:59:17 +0800

    Added a user interface

--- Week7GroupWork/Program.cs
+++ Week7GroupWork/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Week7GroupWork.UserInterface;
+
+var calculator = new CalculatorApp();
+calculator.Start();
