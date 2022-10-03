f6e07254559c1a42dbcef3379ded83840165baf1
Romnic James Joaquino <jromnic101@gmail.com>
2022-10-04 00:15:22 +0800

    i added private string name

---
+++ Week7GroupWork/Entities/Testing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7GroupWork.Entities
+{
+    internal class Testing
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
